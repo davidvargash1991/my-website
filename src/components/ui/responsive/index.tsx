803c12ed243c960b354451267be028ebb66e8157
davidvargash <dvargas@zenfolio.com>
2020-04-11 11:57:57 -0500

    animation fixed

--- src/components/ui/responsive/index.tsx
+++ src/components/ui/responsive/index.tsx
@@ -1,54 +1,58 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { Component } from "react";
 import styles from "./responsive.module.scss";
 import cx from "classnames";
 import responsiveImg from "icons/responsive.png";
 import { ILanguageProps } from "data/strings";
 
-const Responsive: React.FC<ILanguageProps> = ({ strings }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [animateClassImg, setAnimateClassImg] = useState("");
-  const [animateClassText, setAnimateClassText] = useState("");
+interface IResponsiveState {
+  animateClassImg: string;
+  animateClassText: string;
+}
 
-  useEffect(
-    function () {
-      Promise.resolve(
-        typeof window.IntersectionObserver !== "undefined"
-          ? window.IntersectionObserver
-          : import("intersection-observer")
-      ).then(() => {
-        const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0];
-          if (isIntersecting) {
-            setAnimateClassImg(styles.animateImg);
-            setAnimateClassText(styles.animateText);
-            observer.disconnect();
-          }
+class Responsive extends Component<ILanguageProps> {
+  private ref = React.createRef<HTMLImageElement>();
+  public state: IResponsiveState = {
+    animateClassImg: "",
+    animateClassText: "",
+  };
+  private listenToScroll = () => {
+    if (this.ref.current) {
+      if (this.ref.current.getBoundingClientRect().top <= 500) {
+        this.setState({
+          animateClassImg: styles.animateImg,
+          animateClassText: styles.animateText,
         });
-        if (ref.current) {
-          observer.observe(ref.current);
-        }
-      });
-    },
-    [ref]
-  );
-  const endofSection: React.CSSProperties = { height: "1px", width: "1px" };
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <img
-          className={cx(styles.img, animateClassImg)}
-          src={responsiveImg}
-          alt="responsive"
-        />
-        <div>
-          <div className={cx(styles.text, animateClassText)}>
-            {strings.responsiveText}
+      }
+    }
+  };
+  public componentDidMount() {
+    window.addEventListener("scroll", this.listenToScroll);
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener("scroll", this.listenToScroll);
+  }
+  public render() {
+    const { strings } = this.props;
+    const { animateClassImg, animateClassText } = this.state;
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img
+            ref={this.ref}
+            className={cx(styles.img, animateClassImg)}
+            src={responsiveImg}
+            alt="responsive"
+          />
+          <div>
+            <div className={cx(styles.text, animateClassText)}>
+              {strings.responsiveText}
+            </div>
           </div>
         </div>
       </div>
-      <div ref={ref} style={endofSection} />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Responsive;
